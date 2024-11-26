ea7149e78e4ad4e113daf987349d9620d9a9e4f2
Julia <julianyambs18@gmail.com>
2024-11-26 10:55:34 +0300

    Router added

--- Animated-React/vite-project/src/App.jsx
+++ Animated-React/vite-project/src/App.jsx
@@ -1,14 +1,18 @@
 import { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Signup from "./signup.jsx"; // Assuming Signup.js is in the same directory
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div>
-      <Signup />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/register" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ Animated-React/vite-project/src/login.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+function login() {
+  return <div>Login</div>;
+}
+export default login;
